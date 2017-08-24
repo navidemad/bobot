b005dfe2b52a4544e6e6b4f579919cb43e78d302
Navid EMAD <contact@navidemad.net>
2017-08-24 11:02:58 +0200

    [bot] comment event show_typing on/off and mark as seen

--- lib/bobot/commander.rb
+++ lib/bobot/commander.rb
@@ -33,7 +33,7 @@ module Bobot
         event = Bobot::Event.parse(payload)
         hooks.fetch(Bobot::Event::EVENTS.invert[event.class].to_sym)
         puts "[ActiveJob] << Bobot::HookJob with event #{event.class}" if Bobot.debug_log
-        event.mark_as_seen
+        #event.mark_as_seen
         job = Bobot::CommanderJob
         if Bobot.async
           job.perform_later(payload: payload)
@@ -48,9 +48,9 @@ module Bobot
         event = Bobot::Event.parse(payload)
         hook = hooks.fetch(Bobot::Event::EVENTS.invert[event.class].to_sym)
         puts "[ActiveJob] >> Bobot::HookJob related to event #{name.class}" if Bobot.debug_log
-        event.show_typing(state: true)
+        #event.show_typing(state: true)
         hook.call(event)
-        event.show_typing(state: false)
+        #event.show_typing(state: false)
         [event, event.payloads_sent[1..-2]]
       rescue KeyError
         $stderr.puts "Ignoring #{event.class} (no hook registered)"
